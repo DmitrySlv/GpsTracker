cfd4a9cbf35802fed015dda4b7cff410b8b7589d
DmitrySlv <dscreateapp@gmail.com>
2023-01-18 18:55:12 +0400

    Создание нижнего меню. Создание bottom navigation view меню с разметкой bottom_menu.xml и создание строковых ресурсов для названий в меню. Инициализация меню в MainActivity.kt и вывод toast при нажатии на каждую кнопку в меню. Исправление в зависимостях в build.gradle при котором не отображалось bNavView меню в разметке и исправление её на правильное отображение.

--- app/src/main/java/com/dscreate_app/gpstracker/MainActivity.kt
+++ app/src/main/java/com/dscreate_app/gpstracker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dscreate_app.gpstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.dscreate_app.gpstracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        onBottomNavClicks()
+    }
+
+    private fun onBottomNavClicks() {
+        binding.bNavView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.id_home -> {
+                    Toast.makeText(this, "home", Toast.LENGTH_SHORT).show()
+                }
+                R.id.id_tracks -> {
+                    Toast.makeText(this, "tracks", Toast.LENGTH_SHORT).show()
+                }
+                R.id.id_settings -> {
+                    Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 }
