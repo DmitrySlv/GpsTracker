cb64cc2069a45411d300bebf563d6a1b9036d51e
DmitrySlv <dscreateapp@gmail.com>
2023-01-18 15:44:03 +0400

    Создание проекта. Создание разметки с сонстраинт под фрагмент и bottomnavigationView меню. Удаление темной темы и отключение заголовка и action bar

--- app/src/main/java/com/dscreate_app/gpstracker/MainActivity.kt
+++ app/src/main/java/com/dscreate_app/gpstracker/MainActivity.kt
@@ -2,10 +2,14 @@ package com.dscreate_app.gpstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dscreate_app.gpstracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
