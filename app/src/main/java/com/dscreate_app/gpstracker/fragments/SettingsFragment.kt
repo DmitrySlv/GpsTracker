cf0703ced2c7325227c87c70b495978d6100368f
DmitrySlv <dscreateapp@gmail.com>
2023-01-19 20:05:22 +0400

    Настройка SettingsFragment.kt ч2. Добавление логики при которой происходит сохранение выбранного значения времени в preference разметке. Для этого создание  private lateinit var timePref: Preference и в private fun init() - её инициализация со значением из key в разметке preference элемента на который нажали. Также создание private fun onChangeListener(): OnPreferenceChangeListener - которая возвращает листенер с preference и value. Берется два array: nameArray и valueArray. Затем из preference берется title.toString.substringBefore(":") - в котором вычитается и сохраняется все до ":". Чтобы выбранные значения не складывались вместе и не отображались. Затем берется position из valueArray.indexOf(value) и к preference.title = "$title: ${nameArray[position]}" - добавляется заголовок из массива с именем по позиции. И true.

--- app/src/main/java/com/dscreate_app/gpstracker/fragments/SettingsFragment.kt
+++ app/src/main/java/com/dscreate_app/gpstracker/fragments/SettingsFragment.kt
@@ -1,12 +1,41 @@
 package com.dscreate_app.gpstracker.fragments
 
 import android.os.Bundle
+import androidx.preference.Preference
+import androidx.preference.Preference.OnPreferenceChangeListener
 import androidx.preference.PreferenceFragmentCompat
 import com.dscreate_app.gpstracker.R
 
 class SettingsFragment: PreferenceFragmentCompat() {
 
+    private lateinit var timePref: Preference
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main_preference, rootKey)
+        init()
+    }
+
+    private fun init() {
+        timePref = findPreference(UPDATE_TIME_KEY)!!
+        val changeListener = onChangeListener()
+        timePref.onPreferenceChangeListener = changeListener
+    }
+
+    private fun onChangeListener(): OnPreferenceChangeListener {
+        return OnPreferenceChangeListener {
+            preference, value ->
+            val nameArray = resources.getStringArray(R.array.location_time_update_name)
+            val valueArray = resources.getStringArray(R.array.location_time_update_value)
+            val title = preference.title.toString().substringBefore(":")
+            val position = valueArray.indexOf(value)
+            preference.title = "$title: ${nameArray[position]}"
+            true
+        }
+    }
+
+
+    companion object {
+
+        private const val UPDATE_TIME_KEY = "update_time_key"
     }
 }
