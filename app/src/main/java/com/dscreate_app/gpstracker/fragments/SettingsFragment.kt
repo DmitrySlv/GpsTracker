34d21513da845ae6b6d8ee16ddea1cd38e39c87a
DmitrySlv <dscreateapp@gmail.com>
2023-01-20 15:00:55 +0400

    Настройка SettingsFragment.kt ч4. Добавление логики для сохранения и переключения цветов в main_preference.xml и в SettingsFragment.kt Разделение private fun onChangeListener(): OnPreferenceChangeListener слушателя на возможность использования одного кода для переключения времени и цвета.

--- app/src/main/java/com/dscreate_app/gpstracker/fragments/SettingsFragment.kt
+++ app/src/main/java/com/dscreate_app/gpstracker/fragments/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.dscreate_app.gpstracker.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.Preference.OnPreferenceChangeListener
@@ -9,6 +10,7 @@ import com.dscreate_app.gpstracker.R
 class SettingsFragment: PreferenceFragmentCompat() {
 
     private lateinit var timePref: Preference
+    private lateinit var colorPref: Preference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.main_preference, rootKey)
@@ -17,19 +19,20 @@ class SettingsFragment: PreferenceFragmentCompat() {
 
     private fun init() {
         timePref = findPreference(UPDATE_TIME_KEY)!!
+        colorPref = findPreference(COLOR_KEY)!!
         val changeListener = onChangeListener()
         timePref.onPreferenceChangeListener = changeListener
+        colorPref.onPreferenceChangeListener = changeListener
         initPreferences()
     }
 
     private fun onChangeListener(): OnPreferenceChangeListener {
         return OnPreferenceChangeListener {
             preference, value ->
-            val nameArray = resources.getStringArray(R.array.location_time_update_name)
-            val valueArray = resources.getStringArray(R.array.location_time_update_value)
-            val title = preference.title.toString().substringBefore(DELIMITER)
-            val position = valueArray.indexOf(value)
-            preference.title = "$title: ${nameArray[position]}"
+            when (preference.key) {
+                UPDATE_TIME_KEY -> onTimeChange(value.toString())
+                COLOR_KEY -> preference.icon?.setTint(Color.parseColor(value.toString()))
+            }
             true
         }
     }
@@ -39,7 +42,18 @@ class SettingsFragment: PreferenceFragmentCompat() {
         val nameArray = resources.getStringArray(R.array.location_time_update_name)
         val valueArray = resources.getStringArray(R.array.location_time_update_value)
         val title = timePref.title
-        val position = valueArray.indexOf(pref?.getString(UPDATE_TIME_KEY, DEF_VALUE))
+        val position = valueArray.indexOf(pref?.getString(UPDATE_TIME_KEY, DEF_VALUE_TIME))
+        timePref.title = "$title: ${nameArray[position]}"
+
+        val trackColor = pref?.getString(COLOR_KEY, DEF_VALUE_COLOR)
+        colorPref.icon?.setTint(Color.parseColor(trackColor))
+    }
+
+    private fun onTimeChange(value: String) {
+        val nameArray = resources.getStringArray(R.array.location_time_update_name)
+        val valueArray = resources.getStringArray(R.array.location_time_update_value)
+        val title = timePref.title.toString().substringBefore(DELIMITER)
+        val position = valueArray.indexOf(value)
         timePref.title = "$title: ${nameArray[position]}"
     }
 
@@ -47,7 +61,9 @@ class SettingsFragment: PreferenceFragmentCompat() {
     companion object {
 
         private const val UPDATE_TIME_KEY = "update_time_key"
+        private const val COLOR_KEY = "color_key"
         private const val DELIMITER = ":"
-        private const val DEF_VALUE = "3000"
+        private const val DEF_VALUE_TIME = "3000"
+        private const val DEF_VALUE_COLOR = "#03A9F4"
     }
 }
