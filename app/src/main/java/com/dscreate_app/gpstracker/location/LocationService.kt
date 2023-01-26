cfb8ce1302e51e3d805aaeb17c948de6ce6d0d80
DmitrySlv <dscreateapp@gmail.com>
2023-01-26 17:10:24 +0400

    Добавление слушателя обновления местоположения и настройка интервала получения координат для регулировки потребления расхода заряда батареи пользователя. Подключение библиотеки google для получения местоположения.

--- app/src/main/java/com/dscreate_app/gpstracker/location/LocationService.kt
+++ app/src/main/java/com/dscreate_app/gpstracker/location/LocationService.kt
@@ -1,19 +1,28 @@
 package com.dscreate_app.gpstracker.location
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import com.dscreate_app.gpstracker.MainActivity
 import com.dscreate_app.gpstracker.R
+import com.google.android.gms.location.*
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
 
 
 class LocationService: Service() {
+
+    private lateinit var locationProvider: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -21,10 +30,12 @@ class LocationService: Service() {
 
     override fun onCreate() {
         super.onCreate()
+        initLocation()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         startNotification()
+        startLocationUpdates()
         isRunning = true
         return START_STICKY
     }
@@ -32,6 +43,14 @@ class LocationService: Service() {
     override fun onDestroy() {
         super.onDestroy()
         isRunning = false
+        locationProvider.removeLocationUpdates(locationCallback)
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            Log.d("MyLog", "Location: ${locationResult.lastLocation?.latitude}")
+        }
     }
 
     private fun startNotification() {
@@ -58,6 +77,28 @@ class LocationService: Service() {
             .setContentTitle("Gps Tracker running!")
             .setContentIntent(pendingIntent).build()
         startForeground(NOTIFICATION_ID, notification)
+    }
+
+    private fun initLocation() {
+        locationRequest = LocationRequest.create()
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = PRIORITY_HIGH_ACCURACY
+
+        locationProvider = LocationServices.getFusedLocationProviderClient(baseContext)
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        locationProvider.requestLocationUpdates(
+            locationRequest, locationCallback, Looper.myLooper()
+        )
     }
 
     companion object {
